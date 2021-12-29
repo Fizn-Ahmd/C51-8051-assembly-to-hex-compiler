d31d652657451739947b2f15e21747a9ffd12d53
Fizn-Ahmd <faizanahmedfzn@gmail.com>
2021-12-29 17:40:03 +0530

    Add clang-format

--- asmtohex.c
+++ asmtohex.c
@@ -1,13 +1,12 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#include"asmtohex.h"
+#include "asmtohex.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char *argv[])
-{
-    FILE *fp, *fs;
+int main(int argc, char *argv[]) {
+  FILE *fp, *fs;
 
-    fp = fopen(argv[1],"r");
-    fs = fopen("a.hex","w");
-    core_convertion_algorithm(fp, fs);
+  fp = fopen(argv[1], "r");
+  fs = fopen("a.hex", "w");
+  core_convertion_algorithm(fp, fs);
 }
--- asmtohex.h
+++ asmtohex.h
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 void core_convertion_algorithm(FILE *, FILE *);
 void copy(char *, char *);
