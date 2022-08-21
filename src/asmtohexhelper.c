287f53454f9366fff6f537009b013591c2d7d3fb
Fizn-Ahmd <faizanahmedfzn@gmail.com>
2022-08-22 00:02:56 +0530

    Improve naming

--- src/asmtohexhelper.c
+++ src/asmtohexhelper.c
@@ -2,6 +2,9 @@
 
 #include <ctype.h>
 
+#define MAX_CHECKSUM_SIZE 3
+#define MAX_BYTES_IN_A_LINE 16
+
 /* This funtion does the strcpy operation */
 void str_cpy(char *src, char *dest) {
   while (*dest) {
@@ -46,7 +49,7 @@ void str_rev(char *str) {
 
 /* This funtion coverts the string hexadecimal to integer. eg: '0A' is converted
  * to 10. */
-int str_to_int(char *str) {
+int hex_str_to_int(char *str) {
   int i = 0, num = 0;
   while (str[i]) {
     if (str[i] >= '0' && str[i] <= '9') {
@@ -71,108 +74,106 @@ int arr_sum(int *arr, int count) {
 }
 
 /* Calculating the checksum */
-char checksum(char *p, char *q) {
-  char s[3], ch;
-  int a[40], count = 0;
+char checksum(char *addr_str, char *gen_hex_str) {
+  char hex_byte[MAX_CHECKSUM_SIZE];
+  int hex_val[MAX_BYTES_IN_A_LINE];
+  int total_gen_hex_byte = 0;
   int i = 0, j = 0, k = 0;
-  while (p[i]) {
-    s[j] = p[i];
-    j++;
-    i++;
+
+  while (addr_str[i]) {
+    hex_byte[j++] = addr_str[i++];
     if (j == 2) {
-      s[j] = '\0';
+      hex_byte[j] = '\0';
       j = 0;
-      a[k] = str_to_int(s);
-      k++;
+      hex_val[k++] = hex_str_to_int(hex_byte);
     }
   }
   i = 0;
   j = 0;
-  while (q[i]) {
-    s[j] = q[i];
-    j++;
-    i++;
+  while (gen_hex_str[i]) {
+    hex_byte[j++] = gen_hex_str[i++];
     if (j == 2) {
-      s[j] = '\0';
+      hex_byte[j] = '\0';
       j = 0;
-      a[k] = str_to_int(s);
-      k++;
-      count++;
+      hex_val[k++] = hex_str_to_int(hex_byte);
+      total_gen_hex_byte++;
     }
   }
-  a[k] = -1;
-  ch = arr_sum(a, count);
-  return ch;
+  hex_val[k] = -1;
+  return arr_sum(hex_val, total_gen_hex_byte);
 }
 
 /* Writing the generated hex code to the output file */
-void write_to_hex_file(FILE *fs, char *d, char *h) {
-  char ch;
-  ch = checksum(d, h);
-  ch = ~ch;
-  ch = ch + 1;
-  char gi[3], gfx;
-  gfx = ch >> 4 & 0x0f;
-  if (gfx >= 0 && gfx <= 9)
-    gi[0] = (ch >> 4 & 0x0f) + 48;
-  else
-    gi[0] = (ch >> 4 & 0x0f) + 55;
+void write_to_hex_file(FILE *output_fs, char *addr_str, char *hex_gen_str) {
+  char chk_sum, tmp_chk_sum;
+  char final_checksum[MAX_CHECKSUM_SIZE];
 
-  gfx = ch & 0x0F;
+  chk_sum = checksum(addr_str, hex_gen_str);
+  chk_sum = ~chk_sum;
+  chk_sum += 1;
 
-  if (gfx >= 0 && gfx <= 9)
-    gi[1] = (ch & 0x0F) + 48;
-  else
-    gi[1] = (ch & 0x0F) + 55;
+  tmp_chk_sum = chk_sum >> 4 & 0x0f;
+  if (tmp_chk_sum >= 0 && tmp_chk_sum <= 9) {
+    final_checksum[0] = (chk_sum >> 4 & 0x0f) + 48;
+  } else {
+    final_checksum[0] = (chk_sum >> 4 & 0x0f) + 55;
+  }
 
-  gi[2] = '\0';
+  tmp_chk_sum = chk_sum & 0x0F;
+  if (tmp_chk_sum >= 0 && tmp_chk_sum <= 9) {
+    final_checksum[1] = (chk_sum & 0x0F) + 48;
+  } else {
+    final_checksum[1] = (chk_sum & 0x0F) + 55;
+  }
 
-  int i = 0, count = 0;
-  while (h[i]) {
+  final_checksum[2] = '\0';
+
+  int i = 0, byte_size = 0;
+  while (hex_gen_str[i]) {
     if (i % 2 == 0)
-      count++;
+      byte_size++;
     i++;
   }
 
-  fputc(':', fs);
-  if (count >= 0 && count <= 9) {
-    fputc('0', fs);
-    fputc(count + 48, fs);
-  } else if (count >= 10 && count <= 15) {
-    fputc('0', fs);
-    fputc(count + 55, fs);
+  fputc(':', output_fs);
+  if (byte_size >= 0 && byte_size <= 9) {
+    fputc('0', output_fs);
+    fputc(byte_size + 48, output_fs);
+  } else if (byte_size >= 10 && byte_size <= 15) {
+    fputc('0', output_fs);
+    fputc(byte_size + 55, output_fs);
   } else {
-    fputc('1', fs);
-    fputc('0', fs);
+    fputc('1', output_fs);
+    fputc('0', output_fs);
   }
 
-  fputs(d, fs);
-  fputc('0', fs);
-  fputc('0', fs);
-  fputs(h, fs);
-  fputs(gi, fs);
-  fputc(10, fs);
+  fputs(addr_str, output_fs);
+  fputc('0', output_fs);
+  fputc('0', output_fs);
+  fputs(hex_gen_str, output_fs);
+  fputs(final_checksum, output_fs);
+  fputc(10, output_fs); // this adds new line
 }
 
-void check_hex_index(FILE *fs, char *gen_hex_str, char *addr_str, int *index) {
-  if (*index >= MAX_HEX_INDEX) {
-    gen_hex_str[*index] = '\0';
-    *index = 0;
+void check_hex_index(FILE *fs, char *gen_hex_str, char *addr_str, int *hex_index) {
+  if (*hex_index >= MAX_HEX_INDEX) {
+    gen_hex_str[*hex_index] = '\0';
+    *hex_index = 0;
     write_to_hex_file(fs, addr_str, gen_hex_str);
   }
 }
 
 void update_the_gen_hex_str(FILE *fs, char *input_str, char *addr_str,
-                            char *gen_hex_str, int i, int *j) {
+                            char *gen_hex_str, int i, int *hex_index) {
   if (input_str[i + 2] != 'x') {
-    check_hex_index(fs, gen_hex_str, addr_str, j);
-    gen_hex_str[*j++] = input_str[i + 1];
-    gen_hex_str[*j++] = input_str[i + 2];
-    // h[j++] = '\0';
+    check_hex_index(fs, gen_hex_str, addr_str, hex_index);
+    gen_hex_str[*hex_index++] = input_str[i + 1];
+    gen_hex_str[*hex_index++] = input_str[i + 2];
+    // h[hex_index++] = '\0';
   } else {
-    check_hex_index(fs, gen_hex_str, addr_str, j);
-    gen_hex_str[*j++] = input_str[i + 3];
-    gen_hex_str[*j++] = input_str[i + 4];
+    check_hex_index(fs, gen_hex_str, addr_str, hex_index);
+    gen_hex_str[*hex_index++] = input_str[i + 3];
+    gen_hex_str[*hex_index++] = input_str[i + 4];
     // h[4] = '\0';
   }
 }
@@ -180,33 +181,33 @@ void update_the_gen_hex_str(FILE *fs, char *input_str, char *addr_str,
 void update_single_byte() {}
 
 void update_two_bytes(FILE *output_fs, char *gen_hex_str, char *input_str,
-                      char *addr_str, int *j, char op1, char op2, char symb) {
+                      char *addr_str, int *hex_index, char op1, char op2, char symb) {
   check_hex_index(output_fs, gen_hex_str, addr_str, j);
-  gen_hex_str[*j++] = op1;
-  gen_hex_str[*j++] = op2;
+  gen_hex_str[*hex_index++] = op1;
+  gen_hex_str[*hex_index++] = op2;
   int i = 0;
   while (input_str[i]) {
     if (input_str[i] == symb) {
       update_the_gen_hex_str(output_fs, input_str, addr_str, gen_hex_str, i,
-                             j);
+                             hex_index);
     }
     i++;
   }
 }
 
 void update_three_bytes(FILE *output_fs, char *gen_hex_str, char *input_str,
-                        char *addr_str, int *j, char op1, char op2) {
-  check_hex_index(output_fs, gen_hex_str, addr_str, j);
-  gen_hex_str[*j++] = op1;
-  gen_hex_str[*j++] = op2;
+                        char *addr_str, int *hex_index, char op1, char op2) {
+  check_hex_index(output_fs, gen_hex_str, addr_str, hex_index);
+  gen_hex_str[*hex_index++] = op1;
+  gen_hex_str[*hex_index++] = op2;
   int i = 0;
   while (input_str[i]) {
     if (input_str[i] == '#') {
-      check_hex_index(output_fs, gen_hex_str, addr_str, j);
-      gen_hex_str[*j++] = input_str[i - 3];
-      gen_hex_str[*j++] = input_str[i - 2];
+      check_hex_index(output_fs, gen_hex_str, addr_str, hex_index);
+      gen_hex_str[*hex_index++] = input_str[i - 3];
+      gen_hex_str[*hex_index++] = input_str[i - 2];
       update_the_gen_hex_str(output_fs, input_str, addr_str, gen_hex_str, i,
-                             j);
+                             hex_index);
     }
     i++;
   }
